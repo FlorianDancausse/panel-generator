2550701df922cb04cd307c603897a618e6f93c03
Neko <dancausseflorian@gmail.com>
2020-01-14 23:24:29 +0100

    refactor(bloc-size): use of angular/material

--- src/app/modules/generator/generator-components.module.ts
+++ src/app/modules/generator/generator-components.module.ts
@@ -2,6 +2,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 /** end angular */
 
 /** components */
@@ -13,7 +17,7 @@ import { BlocSizeComponent } from './components/generator/components/bloc-size/b
 const components = [GeneratorComponent, BlocBorderComponent, BlocSizeComponent];
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule],
   declarations: components
 })
 export class GeneratorComponentModule {}
