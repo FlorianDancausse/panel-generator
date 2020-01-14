a879b09428da407dd69d8daaf778e79211232330
Neko <dancausseflorian@gmail.com>
2020-01-14 20:54:39 +0100

    build(angular): Add angular-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
+/** angular */
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/** end angular */
 
+/** modules */
 import { AppRoutingModule } from './app-routing.module';
+/** end modules */
+
+/** components */
 import { AppComponent } from './app.component';
 import { BlocSizeComponent } from './shared/bloc-size/bloc-size.component';
 import { BlocBorderComponent } from './shared/bloc-border/bloc-border.component';
+/** components */
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BlocSizeComponent,
-    BlocBorderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, BlocSizeComponent, BlocBorderComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
