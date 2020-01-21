21fac3a575d011ea2b01ae483757a0c14457e204
Neko <Neko>
2020-01-21 23:22:27 +0100

    refactor(panel-size): clean code

--- src/app/modules/generator/components/generator/components/bloc-size/bloc-size.component.ts
+++ src/app/modules/generator/components/generator/components/bloc-size/bloc-size.component.ts
@@ -1,5 +1,5 @@
 /** angular */
-import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 /** end angular */
 
 /** classes */
@@ -13,36 +13,7 @@ import { Panel } from '../../classes/panel';
 })
 export class BlocSizeComponent {
   /**
-   * New width to emit
-   */
-  @Output() widthEmitter = new EventEmitter<number>();
-  /**
-   * New height to emit
-   */
-  @Output() heightEmitter = new EventEmitter<number>();
-
-  /**
    * The current panel
    */
   @Input() panel: Panel;
-
-  constructor() {}
-
-  /**
-   * Emit the new width on change
-   *
-   * @param newWidth Width input value
-   */
-  onChangeWidth(newWidth: number) {
-    this.widthEmitter.emit(newWidth);
-  }
-
-  /**
-   * Emit the new height on change
-   *
-   * @param newHeight Height input value
-   */
-  onChangeHeight(newHeight: number) {
-    this.heightEmitter.emit(newHeight);
-  }
 }
